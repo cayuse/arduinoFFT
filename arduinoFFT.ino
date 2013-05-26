392b60a44b4c1e80c205f2ee25e2aab7aab2b4f3
Mark Schultz <n9xmj@yahoo.com>
2013-05-26 02:57:22 -0700

    Played around with FFT data post-processing.  Perceived response MUCH better.

--- arduinoFFT.ino
+++ arduinoFFT.ino
@@ -29,16 +29,16 @@ void setup() {
     pinMode(adc_pin, INPUT);
 
     pinMode(red_pin, OUTPUT);
-    analogWrite(red_pin, 0);
+    analogWrite(red_pin, 0x00);
 
     pinMode(yellow_pin,OUTPUT);
-    analogWrite(yellow_pin, 0);
+    analogWrite(yellow_pin, 0xFF);
 
     pinMode(green_pin, OUTPUT);
-    analogWrite(green_pin, 0);
+    analogWrite(green_pin, 0xFF);
 
     pinMode(blue_pin, OUTPUT);
-    analogWrite(blue_pin, 0);
+    analogWrite(blue_pin, 0x00);
 }
 
 //------------------------------------------------------------------------------
@@ -48,7 +48,6 @@ void loop() {
     uint16_t val;
     char buf[40];
 
-delay(10000);
 Serial.println("Starting up...");
 
     do {
@@ -60,41 +59,49 @@ Serial.println("Starting up...");
         }
 
         fix_fft(data, im, 7, 0);
-
+/*
         chan_a = 0;
         chan_b = 0;
         chan_c = 0;
         chan_d = 0;
+*/
+        // Simple decay filter
+
+        chan_a -= 16;
+        if (chan_a & 0x8000) chan_a = 0;
+        chan_b -= 16;
+        if (chan_b & 0x8000) chan_b = 0;
+        chan_c -= 16;
+        if (chan_c & 0x8000) chan_c = 0;
+        chan_d -= 16;
+        if (chan_d & 0x8000) chan_d = 0;
 
         // Merge 64 frequency bands into 4 (channels A..D)
 
-        for (i = 0; i < 64; i++) {
-            val = sqrt(data[i] * data[i] + im[i] * im[i]); 
+        for (i = 1; i < 64; i++) {
+//          val = sqrt(data[i] * data[i] + im[i] * im[i]); 
+            val = (data[i] * data[i] + im[i] * im[i]) >> 3;
 
-            if (i < 16) {
-                chan_a += val;
+            if (i < 10) {
+                if (val > chan_a) chan_a = val;
             }
-            else if (i < 32) {
-                chan_b += val;
+            else if (i < 20) {
+                if (val > chan_b) chan_b = val;
             }
-            else if (i < 48) {
-                chan_c += val;
+            else if (i < 50) {
+                if (val > chan_c) chan_c = val;
             }
             else {
-                chan_d += val;
+                if (val > chan_d) chan_d = val;
             }
         }
 
-        // Scale summed channel data down to 8 bits
-        // Divide each by 16, since 16 8-bit data points were summed into each channel
+        // Bound channel data to 8-bit maxint
 
-//sprintf(buf, "A:%03X B:%03X C:%03X D:%03X", chan_a, chan_b, chan_c, chan_d);
-//Serial.println(buf);
-
-        chan_a >>= 4;
-        chan_b >>= 4;
-        chan_c >>= 4;
-        chan_d >>= 4;
+        if (chan_a > 0xFF) chan_a = 0xFF;
+        if (chan_b > 0xFF) chan_b = 0xFF;
+        if (chan_c > 0xFF) chan_c = 0xFF;
+        if (chan_d > 0xFF) chan_d = 0xFF;
 
         // Send channel values to PWM outputs
 
